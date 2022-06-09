25952ac73252b7c5fa24f4375bf64a4ef46d4859
HailMuffins <rafaelsm123@outlook.com>
2022-06-09 19:05:26 -0300

    Tipos Tipagem e Operações

--- Fundamentos/4_Bloco/exercício.js
+++ Fundamentos/4_Bloco/exercício.js
@@ -6,3 +6,22 @@ birthYear = "2030";
 console.log(birthYear);
 birthCity = "Londres";
 console.log(birthCity);
+
+===============================================================================================================================
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(`${typeof patientId}, ${typeof isEnrolled}, ${typeof patientInfo}, ${typeof patientEmail}`);
+
+===============================================================================================================================
+
+const base = 5;
+let height = 8;
+console.log(`${base * height}`);
+const perimiter = (base *2) + (height * 2);
