a1366b55b78e4b85f04a1507da01d3cf4725f05e
HailMuffins <rafaelsm123@outlook.com>
2022-06-09 18:46:26 -0300

    Variáveis JavaScript

---
+++ Fundamentos/4_Bloco/exercício.js
@@ -0,0 +1,8 @@
+const myName = "Rafael";
+const birthCity = "Juazeiro do Norte";
+let birthYear = "1997";
+console.log(`${myName}, ${birthCity}, ${birthYear}`);
+birthYear = "2030";
+console.log(birthYear);
+birthCity = "Londres";
+console.log(birthCity);
