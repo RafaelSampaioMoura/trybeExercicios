f4d7275013b178990c72b40b8580cf502e6d2dcb
HailMuffins <rafaelsm123@outlook.com>
2022-06-09 20:42:38 -0300

    Questão 2

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -6,3 +6,12 @@ console.log(`${valueOne - valueTwo}`);
 console.log(`${valueOne * valueTwo}`);
 console.log(`${valueOne / valueTwo}`);
 console.log(`${valueOne % valueTwo}`);
+
+const firstValue = 80;
+const secondValue = 35;
+
+if(firstValue > secondValue) {
+    console.log(`${firstValue}`);
+} else {
+    console.log(`${secondValue}`);
+}
