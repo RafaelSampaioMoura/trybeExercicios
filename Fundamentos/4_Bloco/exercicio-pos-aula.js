07aac8f3495714a578ae0af3f221b430a321f437
HailMuffins <rafaelsm123@outlook.com>
2022-06-10 16:10:40 -0300

    Questão 11

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -99,3 +99,37 @@ if(valorDeCompra < 0 || valorDeVenda < 0) {
 } else {
     console.log(lucro);
 }
+
+const salarioBruto = 4500.50;
+let inss;
+let ir;
+
+function salarioLiquido(salarioBruto) {
+    if(salarioBruto <= 1556.95) {
+        inss = salarioBruto * 0.08;
+    } else if(salarioBruto <= 2594.92) {
+        inss = salarioBruto * 0.09;
+    } else if(salarioBruto <= 5189.82) {
+        inss = salarioBruto * 0.11;
+    } else {
+        inss = 570.88;
+    }
+
+    let salarioBase = salarioBruto - inss;
+
+    if(salarioBase <= 1903.98) {
+        ir = 0;
+    } else if(salarioBase <= 2826.65) {
+        ir = (salarioBase * 0.075) - 142.80;
+    } else if(salarioBase <= 3751.05) {
+        ir = (salarioBase * 0.15) - 354.80;
+    } else if(salarioBase <= 4884.68) {
+        ir = (salarioBase * 0.225) - 636.13;
+    } else {
+        ir = (salarioBase * 0.275) - 869.36;
+    }
+
+    return salarioBase - ir;
+}
+
+console.log(salarioLiquido(salarioBruto));
