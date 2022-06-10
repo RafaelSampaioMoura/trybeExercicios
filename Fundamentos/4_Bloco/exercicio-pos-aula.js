cb1409391fcefea8922db9866c234851164bd74b
HailMuffins <rafaelsm123@outlook.com>
2022-06-10 15:48:50 -0300

    Questões 4 a 6

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -27,3 +27,39 @@ if(valorUm > valorDois && valorUm > valorTres) {
 } else if(valorTres > valorUm && valorTres > valorDois) {
     console.log(`${valorTres}`);
 }
+
+const valorReferencia = -56;
+
+if(valorReferencia < 0) {
+    console.log("negative");
+} else if (valorReferencia > 0) {
+    console.log("positive");
+} else {
+    console.log("zero");
+}
+
+const anguloUm = 60;
+const anguloDois = 60;
+const anguloTres = 60;
+const triangulo = anguloUm + anguloDois + anguloTres;
+const angulosPositivos = anguloUm > 0 && anguloDois > 0 && anguloTres > 0;
+
+console.log(triangulo === 180 && angulosPositivos);
+
+let pecaXadrez = "PeÃo";
+
+if(pecaXadrez.toLowerCase() === "peão") {
+    console.log("frente, uma casa");
+} else if(pecaXadrez.toLowerCase() === "torre") {
+    console.log("frente e lados, ilimitado");
+} else if(pecaXadrez.toLowerCase() === "cavalos") {
+    console.log("em L");
+} else if(pecaXadrez.toLowerCase() === "bispo") {
+    console.log("nas verticais, ilimitado")
+} else if(pecaXadrez.toLowerCase() === "rainha") {
+    console.log("todas as direções, ilimitado")
+} else if(pecaXadrez.toLowerCase() === "rei") {
+    console.log("todas as direções, uma casa")
+} else {
+    console.log("Opção inválida")
+}
