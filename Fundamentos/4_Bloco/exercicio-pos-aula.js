47037b2ef956997d56f84315920264f1317677f4
HailMuffins <rafaelsm123@outlook.com>
2022-06-13 15:34:15 -0300

    Exercicio pos-aula questão 2

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -3,3 +3,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(let number of numbers) {
     console.log(number);
 }
+
+let sum = 0;
+for(let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+}
+
+console.log(sum);
