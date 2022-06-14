d1d6fb41cc7708b85b64269c74998361b1ac7436
HailMuffins <rafaelsm123@outlook.com>
2022-06-14 19:59:59 -0300

    Aula funções

--- Fundamentos/4_Bloco/exercício.js
+++ Fundamentos/4_Bloco/exercício.js
@@ -37,3 +37,72 @@ let car = {
 for(let info in car) {
     console.log(`${info}: ${car[info]}.`)
 };
+
+//============================================================================
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if(b === 0) {
+        return "Não é possível dividir por zero";
+    } else {
+        return a / b;
+    }
+}
+
+function modulo(a, b) {
+    return a % b;
+}
+
+function biggerThan(a, b) {
+    if(a > b) {
+        return `${a} maior que ${b}`;
+    } else if(a < b) {
+        return `${a} menor que ${b}`;
+    } else {
+        return `${a} e ${b} são iguais`;
+    }
+}
+
+function whosBiggest(a, b, c) {
+    if(a > b && a > c) {
+        return `${a} é o maior`;
+    } else if(b > a && b > c) {
+        return `${b} é o maior`;
+    } else if(c > a && c > b) {
+        return `${c} é o maior`;
+    } else {
+        return "Nenhum número é o maior";
+    }
+}
+
+function positiveOrNegative(a) {
+    if(a > 0) {
+        return `${a} é positivo.`;
+    } else if(a < 0) {
+        return `${a} é negativo.`;
+    } else {
+        return `${a} é igual à zero.`;
+    }
+}
+
+function isTriangle(a, b, c) {
+    if(a < 0 || b < 0 || c < 0) {
+        return `Ângulo(s) inválido(s).`
+    } else if((a + b + c) === 180) {
+        return `São ângulos de um triângulo`;
+    } else {
+        return "Não são ângulos de um triângulo";
+    }
+}
+
