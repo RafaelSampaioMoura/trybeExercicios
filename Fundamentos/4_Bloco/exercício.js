6d8478871ba81e023436b00eefa3928820fae917
HailMuffins <rafaelsm123@outlook.com>
2022-06-14 18:52:05 -0300

    Aula for/in e for/of

--- Fundamentos/4_Bloco/exercício.js
+++ Fundamentos/4_Bloco/exercício.js
@@ -15,3 +15,25 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`);
 
 console.log(`A jogadora possui ${player.golden} medalhas de ouro e ${player.silver} medalhas de prata.`)
+
+//============================================================================
+
+let names = {
+    personOne: "João",
+    personTwo: "Maria",
+    personThree: "Jorge"
+};
+
+for(let name in names) {
+    console.log(`Olá ${names[name]}`);
+};
+
+let car = {
+    model: "A3 Sedan",
+    manufacturer: "Audi",
+    year: 2020
+};
+
+for(let info in car) {
+    console.log(`${info}: ${car[info]}.`)
+};
