e1625e22a1ba8b0407a0237c747671e229208ca2
HailMuffins <rafaelsm123@outlook.com>
2022-06-14 21:51:29 -0300

    Pós-aula I: questão 2 a 5

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -5,3 +5,30 @@ let info = {
 };
 
 console.log(`Bem-vinda, ${info['personagem']}`);
+
+info['recorrente'] = "Sim";
+
+console.log(info);
+
+for(let stuff in info) {
+    console.log(stuff);
+}
+
+for(let stuff in info) {
+    console.log(info[stuff]);
+}
+
+let infoTwo = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+
+for(let stuff in infoTwo) {
+    if(stuff === "recorrente") {
+        console.log(`Ambos recorrentes`);
+        break;
+    }
+    console.log(`${info[stuff]} e ${infoTwo[stuff]}`);
+}
