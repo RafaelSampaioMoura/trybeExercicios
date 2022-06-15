0225ab43b126a8e9e7d658303393cd4f89c7f04b
HailMuffins <rafaelsm123@outlook.com>
2022-06-14 22:13:30 -0300

    Pós-aula II: questão 1

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -1,61 +1,70 @@
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-};
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+// };
 
-console.log(`Bem-vinda, ${info['personagem']}`);
+// console.log(`Bem-vinda, ${info['personagem']}`);
 
-info['recorrente'] = "Sim";
+// info['recorrente'] = "Sim";
 
-console.log(info);
+// console.log(info);
 
-for(let stuff in info) {
-    console.log(stuff);
+// for(let stuff in info) {
+//     console.log(stuff);
+// }
+
+// for(let stuff in info) {
+//     console.log(info[stuff]);
+// }
+
+// let infoTwo = {
+//     personagem: "Tio Patinhas",
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: "O último MacPatinhas",
+//     recorrente: "Sim"
+// }
+
+// for(let stuff in infoTwo) {
+//     if(stuff === "recorrente") {
+//         console.log(`Ambos recorrentes`);
+//         break;
+//     }
+//     console.log(`${info[stuff]} e ${infoTwo[stuff]}`);
+// }
+
+// let leitor = {
+//   nome: "Julia",
+//   sobrenome: "Pessoa",
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: "O Pior Dia de Todos",
+//       autor: "Daniela Kopsch",
+//       editora: "Tordesilhas",
+//     },
+//   ],
+// };
+
+// console.log(
+//   `O livro favorito de ${leitor["nome"]} ${leitor["sobrenome"]}de se chama '${leitor["livrosFavoritos"][0]["titulo"]}'`
+// );
+
+// leitor["livrosFavoritos"].push({
+//   titulo: "Harry Potter e o Prisioneiro de Azkaban",
+//   autor: "JK Rowling",
+//   editora: "Rocco",
+// });
+
+// console.log(leitor);
+
+// console.log(`${leitor["nome"]} tem ${leitor["livrosFavoritos"].length} livros favoritos.`);
+
+let word = "arara";
+
+function isPalindrome(word) {
+    let wordReversed = word.split("").reverse().join("");
+    return wordReversed === word;
 }
 
-for(let stuff in info) {
-    console.log(info[stuff]);
-}
-
-let infoTwo = {
-    personagem: "Tio Patinhas",
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: "O último MacPatinhas",
-    recorrente: "Sim"
-}
-
-for(let stuff in infoTwo) {
-    if(stuff === "recorrente") {
-        console.log(`Ambos recorrentes`);
-        break;
-    }
-    console.log(`${info[stuff]} e ${infoTwo[stuff]}`);
-}
-
-let leitor = {
-  nome: "Julia",
-  sobrenome: "Pessoa",
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: "O Pior Dia de Todos",
-      autor: "Daniela Kopsch",
-      editora: "Tordesilhas",
-    },
-  ],
-};
-
-console.log(
-  `O livro favorito de ${leitor["nome"]} ${leitor["sobrenome"]}de se chama '${leitor["livrosFavoritos"][0]["titulo"]}'`
-);
-
-leitor["livrosFavoritos"].push({
-  titulo: "Harry Potter e o Prisioneiro de Azkaban",
-  autor: "JK Rowling",
-  editora: "Rocco",
-});
-
-console.log(leitor);
-
-console.log(`${leitor["nome"]} tem ${leitor["livrosFavoritos"].length} livros favoritos.`);
+console.log(isPalindrome(word));
