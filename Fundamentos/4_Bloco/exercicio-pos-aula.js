2674952101cd0d02c6455a039019fd1928b9d172
HailMuffins <rafaelsm123@outlook.com>
2022-06-14 22:03:15 -0300

    Pós-aula I: questão 6 a 8

--- Fundamentos/4_Bloco/exercicio-pos-aula.js
+++ Fundamentos/4_Bloco/exercicio-pos-aula.js
@@ -32,3 +32,30 @@ for(let stuff in infoTwo) {
     }
     console.log(`${info[stuff]} e ${infoTwo[stuff]}`);
 }
+
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
+
+console.log(
+  `O livro favorito de ${leitor["nome"]} ${leitor["sobrenome"]}de se chama '${leitor["livrosFavoritos"][0]["titulo"]}'`
+);
+
+leitor["livrosFavoritos"].push({
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+});
+
+console.log(leitor);
+
+console.log(`${leitor["nome"]} tem ${leitor["livrosFavoritos"].length} livros favoritos.`);
