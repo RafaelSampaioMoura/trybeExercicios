d63f248901cc0d976f576d2425d8ef61ae612ef8
HailMuffins <rafaelsm123@outlook.com>
2022-06-17 18:24:39 -0300

    Lição Parte II

--- Fundamentos/4_Bloco/script.js
+++ Fundamentos/4_Bloco/script.js
@@ -6,3 +6,14 @@ console.log(elementoInicial.previousSibling);
 console.log(elementoInicial.nextSibling);
 console.log(elementoInicial.nextElementSibling);
 console.log(elementoInicial.parentNode.childNodes[2]);
+
+const irmaoDoElementoInicial = document.createElement("section");
+elementoInicial.parentElement.appendChild(irmaoDoElementoInicial);
+
+const filhoDoElementoInicial = document.createElement("div");
+elementoInicial.appendChild(filhoDoElementoInicial);
+
+const filhoDoPrimeiroFilhoDoElementoInicial = document.createElement("div");
+elementoInicial.firstElementChild.appendChild(filhoDoPrimeiroFilhoDoElementoInicial);
+
+console.log(elementoInicial.firstElementChild.firstElementChild.parentElement.parentElement.parentElement.children[2]);
