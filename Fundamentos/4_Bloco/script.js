7209872fff498d52ca703d06112dd76d94d421da
HailMuffins <rafaelsm123@outlook.com>
2022-06-21 18:23:51 -0300

    Exercício 1

--- Fundamentos/4_Bloco/script.js
+++ Fundamentos/4_Bloco/script.js
@@ -1,8 +1,14 @@
-const a = 8;
-const b = 2;
+// const a = 8;
+// const b = 2;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+if(a > b) {
+    return a;
+} else {
+    return b;
+}
