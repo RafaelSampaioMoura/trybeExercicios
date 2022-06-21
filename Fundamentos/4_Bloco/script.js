645d553c6b1fbc98303e23b0457a44b0fe7d64ff
HailMuffins <rafaelsm123@outlook.com>
2022-06-21 18:35:08 -0300

    Exercício 8

--- Fundamentos/4_Bloco/script.js
+++ Fundamentos/4_Bloco/script.js
@@ -13,20 +13,28 @@
 //     return b;
 // }
 
-const percentage = 98;
+// const percentage = 98;
 
-if(percentage < 50 && percentage >= 0) {
-    console.log("F");
-} else if(percentage < 60) {
-    console.log("E");
-} else if(percentage < 70) {
-    console.log("D");
-} else if(percentage < 80) {
-    console.log("C");
-} else if(percentage < 90) {
-    console.log("B");
-} else if (percentage <= 100){
-    console.log("A");
-} else {
-    console.log("Erro");
+// if(percentage < 50 && percentage >= 0) {
+//     console.log("F");
+// } else if(percentage < 60) {
+//     console.log("E");
+// } else if(percentage < 70) {
+//     console.log("D");
+// } else if(percentage < 80) {
+//     console.log("C");
+// } else if(percentage < 90) {
+//     console.log("B");
+// } else if (percentage <= 100){
+//     console.log("A");
+// } else {
+//     console.log("Erro");
+// }
+
+const a = 9;
+const b = 8;
+const c = 7;
+
+if (a > 0 && b > 0 && c > 0) {
+  console.log(a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0);
 }
