581620bbc7b4f4ce86c7642c6bdc3427e8cb4d2d
HailMuffins <rafaelsm123@outlook.com>
2022-06-21 18:31:18 -0300

    Exercício 7

--- Fundamentos/4_Bloco/script.js
+++ Fundamentos/4_Bloco/script.js
@@ -7,8 +7,26 @@
 // console.log(a / b);
 // console.log(a % b);
 
-if(a > b) {
-    return a;
+// if(a > b) {
+//     return a;
+// } else {
+//     return b;
+// }
+
+const percentage = 98;
+
+if(percentage < 50 && percentage >= 0) {
+    console.log("F");
+} else if(percentage < 60) {
+    console.log("E");
+} else if(percentage < 70) {
+    console.log("D");
+} else if(percentage < 80) {
+    console.log("C");
+} else if(percentage < 90) {
+    console.log("B");
+} else if (percentage <= 100){
+    console.log("A");
 } else {
-    return b;
+    console.log("Erro");
 }
