789186a6de833117bbdd8b078fe37499fdd6d6f2
HailMuffins <rafaelsm123@outlook.com>
2022-06-28 18:58:49 -0300

    Commit inicial

---
+++ Fundamentos/5_Bloco/aula/script.js
@@ -0,0 +1,11 @@
+const pageTitle = document.getElementById("page-title");
+pageTitle.innerText = "Shawnshawk Redemption";
+
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.style.fontSize = "50px";
+secondParagraph.style.textAlign = "right";
+
+const subtitle = document.getElementById("subtitle");
+subtitle.style.color = "greenyellow";
+subtitle.style.backgroundColor = "orange";
+subtitle.style.width = "65px";
