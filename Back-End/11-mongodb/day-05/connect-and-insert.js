ba5f35064fcfac2430fbd9187ba7d02ee7a2cb0b
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-14 21:57:52 -0300

    exercicios 1-5, módulo mongodb, dia-05

--- Back-End/11-mongodb/day-05/connect-and-insert.js
+++ Back-End/11-mongodb/day-05/connect-and-insert.js
@@ -1,2 +1,10 @@
-
 db = connect("mongodb://localhost:27017");
+
+db.movies.updateOne({ title: "Batman" }, { $push: { category: "superhero" } });
+db.movies.updateOne(
+  { title: "Batman" },
+  { $push: { category: { $each: ["villain", "comic-based"] } } }
+);
+db.movies.updateOne({ title: "Batman" }, { $pull: { category: ["action"] } });
+db.movies.updateOne({ title: "Batman" }, { $pop: { category: -1 } });
+db.movies.updateOne({title: "Batman"}, {$pop: {category: 1}})
