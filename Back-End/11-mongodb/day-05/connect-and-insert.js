b8e6324e6839eccac5edcff2b8a252017800a5b1
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-15 00:31:07 -0300

    exercicios 6-12, módulo mongodb,  dia-05

--- Back-End/11-mongodb/day-05/connect-and-insert.js
+++ Back-End/11-mongodb/day-05/connect-and-insert.js
@@ -7,4 +7,74 @@ db.movies.updateOne(
 );
 db.movies.updateOne({ title: "Batman" }, { $pull: { category: ["action"] } });
 db.movies.updateOne({ title: "Batman" }, { $pop: { category: -1 } });
-db.movies.updateOne({title: "Batman"}, {$pop: {category: 1}})
+db.movies.updateOne({ title: "Batman" }, { $pop: { category: 1 } });
+db.movies.updateOne({ title: "Batman" }, { $addToSet: { category: "action" } });
+db.movies.updateMany(
+  { $or: [{ title: "Batman" }, { title: "Home Alone" }] },
+  { $addToSet: { category: "90's" } }
+);
+
+db.movies.updateOne(
+  { title: "Home Alone" },
+  {
+    $push: {
+      cast: {
+        $each: [
+          { actor: "Macaulay Culkin", character: "Kevin" },
+          { actor: "Joe Pesci", character: "Harry" },
+          { actor: "Daniel Stern" },
+        ],
+      },
+    },
+  }
+);
+
+db.movies.updateOne(
+  { title: "Home Alone", "cast.actor": "Daniel Stern" },
+  { $set: { "cast.$.character": "Marv" } }
+);
+
+db.movies.updateOne(
+  { title: "Batman" },
+  {
+    $push: {
+      cast: {
+        $each: [
+          { character: "Batman" },
+          { character: "Alfred" },
+          { character: "Coringa" },
+        ],
+      },
+    },
+  }
+);
+db.movies.updateOne(
+  { title: "Batman", "cast.character": "Batman" },
+  { $set: { "cast.$.actor": "Christian Bale" } }
+);
+
+db.movies.updateOne(
+  { title: "Batman", "cast.character": "Alfred" },
+  { $set: { "cast.$.actor": "Michael Caine" } }
+);
+
+db.movies.updateOne(
+  { title: "Batman", "cast.character": "Coringa" },
+  { $set: { "cast.$.actor": "Heath Ledger" } }
+);
+
+db.movies.updateOne(
+  { title: "Batman", "cast.character": "Batman" },
+  {
+    $set: {
+      "cast.$actor": {
+        $each: [
+          {
+            "cast.$.actor": ["Michael Keaton", "Val Kilmer", "George Clooney"],
+          },
+        ],
+        $sort: 1,
+      },
+    },
+  }
+);
